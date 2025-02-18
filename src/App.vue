87f43761007e1256c5c781012aa091fee52cdf0e
ce335805 <eckhardt.cj@gmail.com>
2025-02-18 09:36:40 +0100

    publish a test-image

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <div>
-    {{ welcomeMessage}}
+  <div class="test-image-component">
+    <h1>This is a test image</h1>
+    <img src="@/assets/testimage.webp" alt="Test Image" />
   </div>
 </template>
 
 <script setup lang="ts">
-  const welcomeMessage: String = "Hello World!"
 </script>
+
+<style scoped>
+.test-image-component {
+  font-family: sans-serif;
+  text-align: center;
+  padding: 20px;
+}
+
+h1 {
+  margin-bottom: 20px;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  border-radius: 8px;
+}
+</style>
+
